7be630d07c6309e6faa2ecf14293effbcd834a9e
David Julin <david.carl.julin@gmail.com>
2026-05-20 11:44:33 +0200

    now reset assessments work

--- lib/cadet_web/controllers/assessments_controller.ex
+++ lib/cadet_web/controllers/assessments_controller.ex
@@ -37,24 +37,14 @@ defmodule CadetWeb.AssessmentsController do
     end
   end
 
-  def delete_assessment_answers(conn, %{"assessmentid" => assessment_id}, ) do
+  def delete_assessment_answers(conn, %{"assessmentid" => assessment_id}) do
     cr = conn.assigns.course_reg
-    
-    with {:assessment, assessment} when not is_nil(assessment) <-
-          {:assessment, Assessments.get_assessment(assessment_id, cr)},
-        {_count, _} <- Assessments.reset_answers(cr.id, assessment_id) do #returns deleted rows
-      text(conn, "OK")
-
-    else
-      {:assessment, nil} ->
-        conn
-        |> put_status(:not_found)
-        |> text("Assessment not found")
-
-      {:error, {status, message}} ->
-        conn
-        |> put_status(status)
-        |> text(message)
+    case  Assessments.reset_answers(cr.id, assessment_id) do #returns deleted rows
+      {_count, _} -> text(conn, "OK")
+      _ ->
+      conn
+      |> put_status(:internal_server_error)
+      |> text("Something went wrong")
     end
   end
 
